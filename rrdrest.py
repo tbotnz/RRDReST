e76ab89c1ba24ab439a0061260b7db5a7d149c25
tbotnz <tonynealon1989@gmail.com>
2021-06-27 07:15:58 +0000

    alpha-1

--- rrdrest.py
+++ rrdrest.py
@@ -1,8 +1,7 @@
-from fastapi import FastAPI
-from typing import Optional
+from fastapi import FastAPI, HTTPException
 
 from backend.RRD_parse import RRD_parser
-
+import os
 rrd_rest = FastAPI()
 
 
@@ -11,6 +10,9 @@ rrd_rest = FastAPI()
     summary="Get the data from a RRD file, takes in a rrd file path"
     )
 async def get_rrd(rrd_path: str):
-    rr = RRD_parser(rrd_file=rrd_path)
-    r = rr.compile_result()
-    return r
+    is_file = os.path.isfile(rrd_path)
+    if is_file:
+        rr = RRD_parser(rrd_file=rrd_path)
+        r = rr.compile_result()
+        return r
+    raise HTTPException(status_code=404, detail="RRD not found")
